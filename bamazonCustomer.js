b4e4481716e9a8bb602bd1460fd2ac5b1c8304a3
Amanda Dovel <amandacdovel@gmail.com>
2019-05-29 18:23:54 -0600

    update buyer prompt and start function

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,5 +1,7 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
+var colors = require("colors");
+var stars = "\n*********************************\n".rainbow;
 
 // create connection information for mysql database
 var connection = mysql.createConnection({
@@ -20,26 +22,53 @@ connection.connect(function (err) {
 // Include the ids, names, and prices of products for sale.
 
 function start() {
-    connection.query("SELECT * FROM products", function(err, res) {
-        for (var i = 0; i < res.length; i ++) {
-            console.log(res[i].item_id + " | " 
-            + res[i].product_name + " | " 
-            + res[i].department_name +  " | "  
-            + res[i].price + " | "
-            + res[i].stock_quantity + " | " );
+    connection.query("SELECT * FROM products", function (err, res) {
+        console.log(stars);
+        for (var i = 0; i < res.length; i++) {
+            console.log(res[i].item_id + " | "
+                + res[i].product_name + " | "
+                + res[i].department_name + " | "
+                + res[i].price + " | "
+                + res[i].stock_quantity + " | ");
         }
+        console.log(stars);
         if (err) throw err;
-        connection.end();
+     
+        promptBuyer();
     });
 }
 
-// function which asks the user the ID of the product they would like to buys
+// function which asks the user the ID of the product they would like to buy
+// Also asks how much of the product they would like to buy.
+function promptBuyer() {
+    inquirer.prompt([
+        {
+            name: "id",
+            type: "input",
+            message: "What is the ID of the product you would like to buy?"
+        },
+        {
+            name: "quantity",
+            type: "input",
+            message: "How much of the product would you like to purchase?"
+        }
+    ])
+        .then(function (answer) {
+            var productId = answer.id;
+            var productQuantity = answer.quantity;
+            buyer(productId, productQuantity);
+        })
+}
 
-// function which asks how many unite of the product they would like to buy
-
-// Once customer has placed order, check to see if there is enough product to fulfill order
-
-// If not enough, prompt "insufficient quantity", prevent order from going through
+// Check to see if there is enough product to fulfill order
+function buyer(prodId, productQuantity) {
+    connection.query("SELECT * FROM products WHERE item_id = " + prodId, function (err, res) {
+        if (err) throw err;
+        console.log(res);
+    })
+    connection.end();
+}
+// If not enough, prompt "insufficient quantity", prevent  order from going through
 
 // If store does have enough product, decrease quantity by correct amount and reflect remaining amount
 
