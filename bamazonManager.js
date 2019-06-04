7b287ba830607474a5f8c74a24b29c8f3b72ffa8
Amanda Dovel <amandacdovel@gmail.com>
2019-06-03 18:21:30 -0600

    update low inventory and view products

--- bamazonManager.js
+++ bamazonManager.js
@@ -18,18 +18,48 @@ var connection = mysql.createConnection({
 // connect to mysql server and sql database
 connection.connect(function (err) {
     if (err) throw err;
-    start();
+    beginningPrompt();
 });
 
-// Start prompt asks user what they would like to do
+// Prompt that asks user what they would like to do
+function beginningPrompt() {
+    inquirer.prompt([
+        {
+            name: "select",
+            type: "list",
+            message: "What would you like to do?",
+            choices: ["View Products for Sale", "View Low Inventory", "Add to Inventory", "Add New Product"]
+
+        }
+
+// Answer function with switch case for response from user 
+    ]).then(function (answer) {
+        switch (answer.select) {
+            case "View Products for Sale":
+                start();
+                break;
+            case "View Low Inventory":
+                lowInventory();
+                break;
+            case "Add to Inventory":
+                addInventory();
+                break;
+            case "Add New Product":
+                addProduct();
+                break;
+        }
+    })
+}
+
+// Start prompt shows table when user selects 'View products for Sale'
 function start() {
-    connection.query("SELECT * FROM products", function(err, res){
+    connection.query("SELECT * FROM products", function (err, res) {
         console.log(stars);
         products = res;
         var table = new Table({
             head: [colors.bold(colors.magenta('ID')), colors.bold(colors.magenta('Product Name')), colors.bold(colors.magenta('Department')), colors.bold(colors.magenta('Price')), colors.bold(colors.magenta('Quantity'))]
         });
-        
+
         for (var i = 0; i < res.length; i++) {
             table.push([
                 colors.blue(res[i].item_id),
@@ -38,15 +68,43 @@ function start() {
                 colors.cyan(res[i].price),
                 colors.blue(res[i].stock_quantity)
             ])
-            // //console.log(res[i].item_id + " | "
-            //     + res[i].product_name + " | "
-            //     + res[i].department_name + " | "
-            //     + res[i].price + " | "
-            //     + res[i].stock_quantity + " | ");
+
         }
+       // console.log(res);
         console.log(table.toString())
         console.log(stars);
         if (err) throw err;
+        beginningPrompt();
+
+    });
+}
+
+// function for when user selects 'View low inventory'
+function lowInventory() {
+    connection.query("SELECT * FROM products", function (err, res) {
+        console.log(stars);
+        products = res;
+        var table = new Table({
+            head: [colors.bold(colors.magenta('ID')), colors.bold(colors.magenta('Product Name')), colors.bold(colors.magenta('Department')), colors.bold(colors.magenta('Price')), colors.bold(colors.magenta('Quantity'))]
+        });
+
+        for (var i = 0; i < res.length; i++) {
+            if (res[i].stock_quantity < 5) {
+                table.push([
+                    colors.blue(res[i].item_id),
+                    colors.cyan(res[i].product_name),
+                    res[i].department_name,
+                    colors.cyan(res[i].price),
+                    colors.blue(res[i].stock_quantity)
+                ])
+            }
+
+        }
+       
+        console.log(table.toString())
+        console.log(stars);
+        if (err) throw err;
+        beginningPrompt();
 
     });
 }
